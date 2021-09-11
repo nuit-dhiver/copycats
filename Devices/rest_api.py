64046716e294ebc12c989316c4769c6a14c25f6c
Mohammad-Hossein Emami <emami@outlook.com>
2021-09-12 01:07:35 +0430

    Added humidity sensor.

--- Devices/rest_api.py
+++ Devices/rest_api.py
@@ -2,14 +2,26 @@ import json
 from flask import Flask, request,jsonify
 import time
 from Sensors.light_sensor import light_sensor
+from Sensors.humidity_sensor import humidity_sensor
 
-light = light_sensor()
 
 app = Flask(__name__)
 @app.route('/light')
 
-def index():
-    record = sensor.send()
+def get_light():
+
+    light = light_sensor()
+
+    record = light.send()
+    return jsonify(record)
+
+@app.route('/humidity/reg=<region>_app=<application>')
+
+def get_humidity(region='normal',application='outdoor'):
+
+    humidity = humidity_sensor(region=region, application=application)
+
+    record = humidity.send()
     return jsonify(record)
 
 app.run()
